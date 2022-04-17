3e60db9df1eef56fd7859b17a122578cfbb92fb2
hierarch <riichinomics@gmail.com>
2022-04-18 01:44:09 +1200

    Linking json lib correctly

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 // Simple C program to display "Hello World"
-  
+
 // Header file for input output functions
 #include <stdio.h>
-#include <json_object.h>
-  
+#include "json.h"
+
 // main function -
 // where the execution of program begins
 int main()
 {
-	printf(json_object_to_json_string(json_object_new_string("Hello World")));
-
+	printf("Hello World\r\n");
+	fflush(stdout);
+	json_object * test_string = json_object_new_string("Hello World");
+	const char * json_blob = json_object_to_json_string(test_string);
+	printf(json_blob);
 	return 0;
 }
